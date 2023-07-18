9855136c93973a3ea3683cfdc43730c92f308dbc
James McKee <james.mckee@kainos.com>
2023-07-18 10:56:49 +0100

    Initial commit - DropWizard and Swagger set up

---
+++ src/main/java/org/kainos/ea/all_tables_apiApplication.java
@@ -0,0 +1,36 @@
+package org.kainos.ea;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
+public class all_tables_apiApplication extends Application<all_tables_apiConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new all_tables_apiApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "all_tables_api";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<all_tables_apiConfiguration> bootstrap) {
+        bootstrap.addBundle(new SwaggerBundle<all_tables_apiConfiguration>(){
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(all_tables_apiConfiguration configuration) {
+                return configuration.getSwagger();
+            }
+        });
+    }
+
+    @Override
+    public void run(final all_tables_apiConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
