854eadfde3ea52fc447cbb4b2f667cca2b8ee29c
Joshua Young <joshua.young@kainos.com>
2023-07-19 16:26:47 +0100

    TrainingCourse model

---
+++ src/main/java/org/kainos/ea/cli/TrainingCourse.java
@@ -0,0 +1,28 @@
+package org.kainos.ea.cli;
+
+public class TrainingCourse {
+    private String name;
+    private String description;
+
+    public TrainingCourse(String name, String description) {
+        this.setName(name);
+        this.setDescription(description);
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
