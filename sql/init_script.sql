603e4cd057bb85aa68fff444ca460ed7204952fe
Joshua Young <joshua.young@kainos.com>
2023-07-19 13:34:01 +0100

    sql scripts for capabilities

--- sql/init_script.sql
+++ sql/init_script.sql
@@ -5,22 +5,31 @@ BEGIN
 	START TRANSACTION;
 
 
+    DROP TABLE IF EXISTS bands;
+    DROP TABLE IF EXISTS job_roles;
+    DROP TABLE IF EXISTS capabilities;
+
     CREATE TABLE IF NOT EXISTS bands (
         id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         level SMALLINT UNSIGNED NOT NULL
     );
 
-    
+    CREATE TABLE IF NOT EXISTS capabilities (
+        id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        description VARCHAR(255) NOT NULL
+    );
+
 	CREATE TABLE IF NOT EXISTS job_roles (
         id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         specification VARCHAR(255) NOT NULL,
         band_id SMALLINT UNSIGNED NOT NULL,
-        FOREIGN KEY (band_id) REFERENCES bands(id)
+        capability_id SMALLINT UNSIGNED NOT NULL,
+        FOREIGN KEY (band_id) REFERENCES bands(id),
+        FOREIGN KEY (capability_id) REFERENCES capabilities(id)
     );
-
-
 	
 	
 END $$
