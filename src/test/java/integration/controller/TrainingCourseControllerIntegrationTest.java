43b7a980fea44fd1854f7c3560e13237aedd684c
Joshua Young <joshua.young@kainos.com>
2023-07-24 14:58:22 +0100

    training cource controller int test

---
+++ src/test/java/integration/controller/TrainingCourseControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package integration.controller;
+
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.all_tables_apiApplication;
+import org.kainos.ea.all_tables_apiConfiguration;
+import org.kainos.ea.models.TrainingCourse;
+
+import java.util.List;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class TrainingCourseControllerIntegrationTest {
+
+    static final DropwizardAppExtension<all_tables_apiConfiguration> APP = new DropwizardAppExtension<>(
+            all_tables_apiApplication.class, null,
+            new ResourceConfigurationSourceProvider()
+    );
+
+    @Test
+    void getAllTrainingCourses_shouldReturnList() {
+        List<TrainingCourse> response = APP.client().target("http://localhost:8080/api/trainging-courses")
+                .request()
+                .get(List.class);
+
+        Assertions.assertTrue(response.size() > 0);
+    }
+}
