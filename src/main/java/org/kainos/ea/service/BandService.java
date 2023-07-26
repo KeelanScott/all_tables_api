beefcbc26b3a092d6ed4392413412ea3dce8ec87
Joshua Young <joshua.young@kainos.com>
2023-07-26 10:09:52 +0100

    getAllBands

---
+++ src/main/java/org/kainos/ea/service/BandService.java
@@ -0,0 +1,26 @@
+package org.kainos.ea.service;
+
+import org.kainos.ea.exception.DatabaseConnectionException;
+import org.kainos.ea.model.Band;
+import org.kainos.ea.dao.BandDao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BandService {
+
+    private BandDao bandDao;
+
+    public BandService(BandDao bandDao) {
+        this.bandDao = bandDao;
+    }
+
+    public List<Band> getAllBands() throws FailedToGetBandException, DatabaseConnectionException {
+        try {
+            return bandDao.getAllBands();
+        } catch (SQLException e) {
+            throw new FailedToGetBandException();
+        }
+    }
+
+}
