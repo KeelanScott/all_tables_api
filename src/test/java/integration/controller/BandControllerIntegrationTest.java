237d91288d54339b9f22c097558f1d4c5b537863
James McKee <james.mckee@kainos.com>
2023-07-26 12:34:43 +0100

    Remove tests for removed APIs

--- src/test/java/integration/controller/BandControllerIntegrationTest.java
+++ src/test/java/integration/controller/BandControllerIntegrationTest.java
@@ -8,11 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.all_tables_apiApplication;
 import org.kainos.ea.all_tables_apiConfiguration;
-import org.kainos.ea.model.Band;
 import org.kainos.ea.model.BandRequest;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class BandControllerIntegrationTest {
@@ -23,28 +21,11 @@ public class BandControllerIntegrationTest {
     );
 
     @Test
-    void getBands_shouldReturnList() {
-        List<Band> response = APP.client().target("http://localhost:8080/api/bands")
-                .request()
-                .get(List.class);
-
-        Assertions.assertTrue(response.size() > 0);
-    }
-
-    @Test
     void createBand_shouldReturnId(){
         int response = APP.client().target("http://localhost:8080/api/bands")
                 .request()
                 .post(Entity.json(new BandRequest("Test Band", "Level", "responsibilities")), Response.class)
                 .readEntity(Integer.class);
-        Assertions.assertNotNull(response);
-    }
-
-    @Test
-    void getBand_shouldReturnBand(){
-        Band response = APP.client().target("http://localhost:8080/api/bands/1")
-                .request()
-                .get(Band.class);
         Assertions.assertNotNull(response);
     }
 }
